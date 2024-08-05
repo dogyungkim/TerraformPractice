74b559c6025de954caabe182a30fb34dd12b86fc
dogyungkim <prunsoli12@gmail.com>
2024-08-05 11:17:35 +0900

    test1:plan

---
+++ main.tf
@@ -0,0 +1,48 @@
+####기본적인 terraform setup을 위한 tf 파일
+# S3 버켓과 DynamoDB를 생성한다
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "ktb-moviechabot-tfstate-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket        = aws_s3_bucket.terraform_state.bucket 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "this" {
+  name         = "ktb-moviechatbot-tfstate-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
